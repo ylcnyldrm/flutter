5bc4b72756d24386a5532a69a0291036bb5f7db8
Pc\Yalcin <yalcinylde@gmail.com>
2021-03-17 21:08:09 +0200

    stream yapısı için ilk örnek bitti (counter)

--- lib/ikinci_sayfa.dart
+++ lib/ikinci_sayfa.dart
@@ -1,62 +1,71 @@
+import 'dart:async';
+import 'dart:core';
+
 import 'package:flutter/material.dart';
 import 'package:flutterstatemanagement/Counter.dart';
 import 'package:flutterstatemanagement/auth_islemleri.dart';
 import 'package:provider/provider.dart';
 
 class IkinciSayfa extends StatefulWidget {
+
   @override
   _IkinciSayfaState createState() => _IkinciSayfaState();
 }
 
 class _IkinciSayfaState extends State<IkinciSayfa> {
+   
+  final StreamController<int> _streamController = StreamController();
+  int _counter = 0;
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _streamController.close();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("başlık"),
       ),
-      body: Mycolum(),
-      floatingActionButton: myfloatingactionbuttons(),
-    );
-    debugPrint("ıkınc sayfa  BUİLD ");
-  }
-}
-
-class Mycolum extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-
-
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            "selam",
-          ), 
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "selam",
+            ),
+            //
+            StreamBuilder<int>(
+              initialData: 0,
+              stream: _streamController.stream,
+              builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
+                return Text("${snapshot.data}");
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+              heroTag: 1,
+              onPressed: () {
+                _streamController.sink.add(++_counter);
+              }),
+          FloatingActionButton(
+            onPressed: () {
+              _streamController.sink.add(--_counter);
+            },
+            heroTag: 2,
+          ),
         ],
       ),
     );
-  }
-}
-
-class myfloatingactionbuttons extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: [
-        FloatingActionButton(
-            heroTag: 1,
-            onPressed: () {
-            }),
-        FloatingActionButton(
-          onPressed: () {
-          },
-          heroTag: 2,
-        ),
-      ],
-    );
+    debugPrint("ıkınc sayfa  BUİLD ");
   }
 }
